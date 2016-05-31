3db33a76a86944e39353eda11c7886a58269bbf1
lemaypy <lemaypy@gmail.com>
2016-05-31 07:03:58 -0400

    5.20 public key corrected

--- installvirtualbox.sh
+++ installvirtualbox.sh
@@ -1,9 +1,9 @@
 echo "Please enter username: "
 read username
-sudo echo "deb http://download.virtualbox.org/virtualbox/debian wily contrib" >> /etc/apt/sources.list
-sudo wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install -y virtualbox-5.0
+sudo echo "deb http://download.virtualbox.org/virtualbox/debian xenial contrib" > /etc/apt/sources.list.d/virtualbox.list
+sudo wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+sudo apt update
+sudo apt install -y --allow-unauthenticated virtualbox-5.0
 sudo wget http://download.virtualbox.org/virtualbox/5.0.20/Oracle_VM_VirtualBox_Extension_Pack-5.0.20-106931.vbox-extpack
 sudo VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-5.0.20-106931.vbox-extpack
 sudo usermod -aG vboxusers $username
