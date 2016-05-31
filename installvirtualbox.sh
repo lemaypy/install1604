2a7ef009de7ce2d297803533bd2295d1f5c7ac76
lemaypy <lemaypy@gmail.com>
2016-05-31 06:39:30 -0400

    5.20

--- installvirtualbox.sh
+++ installvirtualbox.sh
@@ -4,7 +4,8 @@ sudo echo "deb http://download.virtualbox.org/virtualbox/debian wily contrib" >>
 sudo wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install -y virtualbox-5.0
-sudo wget http://download.virtualbox.org/virtualbox/5.0.14/Oracle_VM_VirtualBox_Extension_Pack-5.0.14-105127.vbox-extpack
-sudo VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-5.0.14-105127.vbox-extpack
+sudo wget http://download.virtualbox.org/virtualbox/5.0.20/Oracle_VM_VirtualBox_Extension_Pack-5.0.20-106931.vbox-extpack
+sudo VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-5.0.20-106931.vbox-extpack
 sudo usermod -aG vboxusers $username
-sudo rm Oracle_VM_VirtualBox_Extension_Pack-5.0.14-105127.vbox-extpack
+sudo rm Oracle_VM_VirtualBox_Extension_Pack-5.0.20-106931.vbox-extpack
+
