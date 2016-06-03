e4bd5894878f880105b76d362e387e3c0150b55a
lemaypy <lemaypy@gmail.com>
2016-06-03 06:28:16 -0400

    add env vars

--- install-x2go.sh
+++ install-x2go.sh
@@ -3,6 +3,9 @@
 #install x2go en silence
 read -p "started with sudo? If not press Ctrl-C"
 
+echo "Please enter username: "
+read username
+
 apt install software-properties-common
 add-apt-repository ppa:x2go/stable
 read -p "edit sources.list to retro to wily then press enter..."
@@ -14,5 +17,6 @@ apt install x2goclient
 #patch pour un serveur
 read -p "run server with XFCE? Press Enter. Otherwise press Ctrl-C"
 
-export GSETTINGS_SCHEMA_DIR=/usr/share/xfce:/usr/share/xfce:/usr/local/share/:/usr/share/:/var/lib/snapd/desktop
-export XDG_DATA_DIRS=/usr/share/xfce:/usr/share/xfce:/usr/local/share/:/usr/share/:/var/lib/snapd/desktop
+echo "export GSETTINGS_SCHEMA_DIR=/usr/share/xfce:/usr/share/xfce:/usr/local/share/:/usr/share/:/var/lib/snapd/desktop" >> /home/$username/.profile
+echo "export XDG_DATA_DIRS=/usr/share/xfce:/usr/share/xfce:/usr/local/share/:/usr/share/:/var/lib/snapd/desktop" >> /home/$username/.profile
+
